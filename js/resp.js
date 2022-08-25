b13eaff397e5952f49cff800f293a33701ac0a87
shriniket <shriniket>
2022-08-25 19:36:09 +0530

    Initial commit

---
+++ js/resp.js
@@ -0,0 +1,11 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navlist = document.querySelector('.nav-list')
+rightnav = document.querySelector('.rightnav')
+
+
+burger .addEventListener('click', ()=>{
+    rightnav.classList.toggle('v-class-resp')
+    navlist.classList.toggle('v-class-resp')
+    navbar.classList.toggle('h-nav-resp')
+})
